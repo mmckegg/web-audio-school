cb2eae816453600b5425b8a6394be12614ca020e
Matt McKegg <matt@wetsand.co.nz>
2015-04-22 17:03:25 +1200

    lessons: change the key to C

--- lessons/1. Subtractive Synthesis/02. Play a short sequence of notes/answer.js
+++ lessons/1. Subtractive Synthesis/02. Play a short sequence of notes/answer.js
@@ -2,9 +2,9 @@
 
 var audioContext = new AudioContext()
 
-play(0, 0, 0.5)
-play(1, 7, 0.5)
-play(2, 12, 0.5)
+play(0, 3, 0.5)
+play(1, 10, 0.5)
+play(2, 15, 0.5)
 
 function play(delay, pitch, duration) {
   var time = audioContext.currentTime + delay
