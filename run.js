898f5097359b154bc514a2d10a4d289249a1e9d6
skratchdot <git@skratchdot.com>
2017-12-10 23:09:50 -0500

    use `opn` for cross-os compatibility. fixes #21

--- run.js
+++ run.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 var exec = require('child_process').exec
+var opn = require('opn');
 
 process.chdir(__dirname)
 var runner = exec('npm start')
@@ -7,7 +8,7 @@ var runner = exec('npm start')
 console.log('Starting server...')
 setTimeout(function () {
   console.log('Go to http://localhost:9966')
-  exec('open http://localhost:9966')
+  opn('http://localhost:9966')
   runner.stdout.pipe(process.stdout)
   runner.stderr.pipe(process.stderr)
 }, 2000)
